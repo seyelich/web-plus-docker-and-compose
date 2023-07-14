69fece655878cb90515635a8a5c0d4dd0f84941e
seyelich <robot11148@gmail.com>
2023-07-14 20:43:25 +0700

    bugfix: change domains

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,8 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: [
-    'http://kupipodariday-seyelic.nomoredomains.work',
-    'https://kupipodariday-seyelic.nomoredomains.work'
+    'http://kupipodariday-seyelich.nomoredomains.work',
+    'https://kupipodariday-seyelich.nomoredomains.work'
   ] });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
